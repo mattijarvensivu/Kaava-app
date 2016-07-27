5f8a1e2014ec4691d640fd7e2175cd24093a3ae1
mattijarvensivu <matti.jarvensivu93@gmail.com>
2016-07-27 19:28:18 +0300

    Math activityn luonti

--- app/src/main/java/com/example/maza/kaavojapp/MathActivity.java
+++ app/src/main/java/com/example/maza/kaavojapp/MathActivity.java
@@ -1,9 +1,14 @@
 package com.example.maza.kaavojapp;
 
+import android.content.Context;
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -13,13 +18,36 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.SimpleCursorAdapter;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Set;
 
 public class MathActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
+    SqlHandler hand;
+    private ListView listView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        // register listView's context menu (to delete row)
+        // registerForContextMenu(listView);
+
+        // get data with own made queryData method
+
         setContentView(R.layout.activity_math);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
@@ -41,6 +69,18 @@ public class MathActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+        listView = (ListView)  findViewById(R.id.lsvTulos);
+        //lisätää listViewiin tapahtuma kun klikataan jotakin sen kohtaa
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Context con = getApplicationContext();
+                ViewGroup prnt = (ViewGroup) findViewById(R.id.lnlContainer); //haetaan isäntä, eli komponentti mihin tuo tiedot sisältävä komponentti tulee
+                placeToCenter(((Tulos) parent.getItemAtPosition(position)).getLargeView((LayoutInflater) con.getSystemService(Context.LAYOUT_INFLATER_SERVICE), prnt));
+            }
+        });
+        hand = new SqlHandler(getApplicationContext().getApplicationContext(), "", null, 1, true);
     }
 
     @Override
@@ -83,7 +123,6 @@ public class MathActivity extends AppCompatActivity
 
         if (id == R.id.nav_camera) {
 
-
         } else if (id == R.id.nav_gallery) {
             Intent myIntent = new Intent(this, PhysicsActivity.class);
             startActivity(myIntent);
@@ -91,8 +130,9 @@ public class MathActivity extends AppCompatActivity
         } else if (id == R.id.nav_slideshow) {
             Intent myIntent = new Intent(this, ChemistryActivity.class);
             startActivity(myIntent);
-
         } else if (id == R.id.nav_manage) {
+
+
 
         } else if (id == R.id.nav_share) {
 
@@ -105,9 +145,143 @@ public class MathActivity extends AppCompatActivity
         finish();
         return true;
     }
-    public void takaisinmath(View v)
-    {
-        Intent myIntent = new Intent(this, MainActivity.class);
-        startActivity(myIntent);
+
+    public void HaeMath(View v) {
+        Log.w("myApp", " Matikka Nappia painettu");
+
+        EditText haku = (EditText) findViewById(R.id.Mathsearch);
+        String hakuparametri = haku.getText().toString();
+        String[] listOfTagTables = new String[]{"Kaava","Vakio"};
+        String[] listOfTables = new String[]{"Kaava","Muuttuja","Vakio"};
+
+        Boolean tarkistus= false;
+        StringValidator val = new StringValidator();
+        tarkistus = val.CheckString(hakuparametri);
+
+        if(tarkistus) {
+
+            // Tarkistus mistä taulusta haetaan täytyy tehä
+            ArrayList<Tulos> tulos = suoritaHaku(hakuparametri,listOfTagTables,true);
+            if(tulos.size()==0 && hakuparametri.length()!=0) {
+                tulos = suoritaHaku(hakuparametri, listOfTables, false);
+            }
+            //Jos haku tyhjä haetaan osahaulla
+            if(tulos.size()==0 && hakuparametri.length()!=0){
+                tulos = suoritaHaku("%"+hakuparametri+"%",listOfTables,false);
+               // for(int i = 0; i < tulos.size(); i++)
+               // {
+                //    if(tulos.get(i).getType() == 1)
+                 //   {
+                 //       ((alkuaineTulos)tulos.get(i)).boldaa(hakuparametri);
+                 //   }
+               // }
+            }
+
+            placeToCenter(listView); //laitetaan listViewi keskelle
+
+            //lisätään tiedot listViewiin näkyville
+            resultAdapter a = new resultAdapter(getApplicationContext(), -1, tulos);
+            listView.setAdapter(a);
+
+            //onClick oli tässä
+
+
+        }else{ Log.w("myApp", tarkistus.toString() );}
     }
+
+    private ArrayList<Tulos> suoritaHaku(String hakuparametri, String[] taulut, boolean isTag)
+    {
+        HashMap<String, String> kentat;
+        String tmpHakuPreP = hakuparametri;
+        String tmpHakuPosP = hakuparametri.replaceAll(", ", ",");
+        while(tmpHakuPreP.compareTo(tmpHakuPosP) != 0)
+        {
+            tmpHakuPreP = tmpHakuPosP;
+            tmpHakuPosP = tmpHakuPosP.replaceAll(", ", ",");
+        }
+        tmpHakuPreP = tmpHakuPosP.replaceAll(" ,", ",");
+        while(tmpHakuPreP.compareTo(tmpHakuPosP) != 0)
+        {
+            tmpHakuPreP = tmpHakuPosP;
+            tmpHakuPosP = tmpHakuPosP.replaceAll(" ,", ",");
+        }
+        ArrayList<HashMap<String, String>> kentatAL = new ArrayList<>();
+        Set<String> kentatNimet;
+        String[] splitattuHP = tmpHakuPosP.split(",");
+        ArrayList<Tulos> tulos = new ArrayList<>();
+
+        for(String t : taulut)
+        {
+            kentatAL.clear();
+            kentat = hand.getDefaultMap(t);
+            if(kentat.size() > 0) {
+                kentatNimet = kentat.keySet();
+                if(!isTag) {
+                    for (String k : kentatNimet) {
+                        kentat.put(k, hakuparametri);
+                    }
+                }else{
+                    //kyseessä on tägi haku
+                    for(String s: splitattuHP) {
+                        HashMap<String, String> tmp = new HashMap<>();
+                        for (String k : kentatNimet) {
+                            while(s.charAt(0) == ' ')
+                            {
+                                s = s.substring(1,s.length());
+                            }
+
+                            while(s.charAt(s.length()-1) == ' ')
+                            {
+                                s = s.substring(0,s.length()-1);
+                            }
+
+                            tmp.put(k, s);
+                        }
+                        kentatAL.add(tmp);
+                    }
+                }
+
+                if(isTag==true)
+                {
+                    ArrayList<HashMap<String,String>> tagit = new ArrayList<>();
+                    if(t.compareTo("Kaava") == 0 || t.compareTo("Vakio") == 0)
+                    {
+                        HashMap<String, String> tmp = new HashMap<>();
+                        tmp.put("nimi","matematiikka");
+                        tagit.add(tmp);
+                    }
+                    tulos.addAll(hand.getValueByTag(t, kentatAL, tagit));
+                }else {
+                    ArrayList<HashMap<String,String>> tagit = new ArrayList<>();
+                    if(t.compareTo("Kaava") == 0 || t.compareTo("Vakio") == 0)
+                    {
+                        HashMap<String, String> tmp = new HashMap<>();
+                        tmp.put("nimi","matematiikka");
+                        tagit.add(tmp);
+                    }
+                    tulos.addAll(hand.getValue(t, kentat,tagit));
+                }
+
+                // tulos.addAll(hand.getValue(t, kentat));
+            }
+
+        }
+
+
+        return tulos;
+
+    }
+
+
+    //Tässä laitetaan annettu Vievi keskellä olevaan layouttiin
+    private void placeToCenter(View target)
+    {
+        LinearLayout contai = (LinearLayout) findViewById(R.id.lnlContainer);
+        contai.removeAllViews();
+        contai.addView(target);
+    }
+
+
+
 }
+
